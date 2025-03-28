5fd89a67e6633ca1e26fb0b8c17889f21b5dd4cc
Vishal Singh <vishal267s@gmail.com>
2025-03-29 02:30:31 +0530

    ok

---
+++ app/services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNGU3OGZiZDJjYThhOTVhNTE5ZmM2MzgwN2YxNzA1NiIsIm5iZiI6MTc0MjE5OTc0NC4zMTMsInN1YiI6IjY3ZDdkYmMwMTkxODY4YzU0ZmYxYzMxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wpq6cXCkJejSvPkMnAeI-eP2MakKSzcYINYltTHiM_o'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
